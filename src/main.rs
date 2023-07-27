ea2abedc0b6d183f7dac839bcf11558e04499cd1
Sebastian Jara <s.jaranilsson@gmail.com>
2023-07-26 21:10:14 -0400

    added a few more route for answers and questions. missing cleaning up codebase

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,14 @@ use tokio::sync::RwLock;
 #[derive(Clone)]
 struct Store {
     questions: Arc<RwLock<HashMap<QuestionId, Question>>>,
+    answers: Arc<RwLock<HashMap<AnswerId, Answer>>>,
 }
 
 impl Store {
     fn new() -> Self {
         Store {
             questions: Arc::new(RwLock::new(Self::init())),
+            answers: Arc::new(RwLock::new(HashMap::new())),
         }
     }
     fn init() -> HashMap<QuestionId, Question> {
@@ -42,6 +44,16 @@ struct Question {
 
 #[derive(Debug, Clone, Serialize, PartialEq, Eq, Hash, Deserialize)]
 struct QuestionId(String);
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq, Hash, Deserialize)]
+struct AnswerId(String);
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Answer {
+    id: AnswerId,
+    content: String,
+    question_id: QuestionId
+}
 
 #[derive(Debug)]
 struct Pagination {
@@ -94,6 +106,21 @@ fn extract_pagination (
     Err(Error::MissingParameters)
 }
 
+async fn add_answer(
+    store: Store,
+    params: HashMap<String, String>
+) -> Result<impl Reply, Rejection> {
+    let answer = Answer {
+        id: AnswerId(store.answers.read().await.len().to_string()),
+        content: params.get("content").unwrap().to_string(),
+        question_id: QuestionId(params.get("question_id").unwrap().to_string()),
+    };
+
+    store.answers.write().await.insert(answer.id.clone(), answer);
+
+    Ok(warp::reply::with_status("Answer added", StatusCode::OK))
+}
+
 async fn add_question(
     store: Store,
     question: Question
@@ -118,6 +145,19 @@ async fn update_question(
         "Question modified",
         StatusCode::OK,
     ))
+}
+
+async fn delete_question(
+    id: String,
+    store: Store
+) -> Result<impl Reply, Rejection> {
+    match store.questions.write().await.remove_entry(&QuestionId(id)) {
+        Some((id, _)) => return Ok(warp::reply::with_status(
+            format!("Question with id {} removed", id.0),
+            StatusCode::OK,
+        )),
+        None => return Err(Error::QuestionNotFound.into())
+    }
 }
 
 async fn get_questions(
@@ -154,7 +194,7 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
         ))
     } else if let Some(error) = r.find::<BodyDeserializeError>() {
         Ok(warp::reply::with_status(
-            error.to_string(),
+            format!("{}, Please try again later!", error.to_string()),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
@@ -204,10 +244,28 @@ async fn main() {
         .and(warp::body::json())
         .and_then(update_question);
 
+    let delete_question =
+        warp::delete()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(delete_question);
+
+    let add_answer =
+        warp::post()
+        .and(warp::path("answers"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::form())
+        .and_then(add_answer);
+
     let routes =
         get_questions
         .or(add_questions)
         .or(update_question)
+        .or(add_answer)
+        .or(delete_question)
         .with(cors)
         .recover(return_error);
 
